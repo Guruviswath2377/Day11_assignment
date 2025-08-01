3bddd1e554c383424cb8fcfa6bd80b70b0ae2158
Guruviswath2377 <guruviswath23@gmail.com>
2025-08-01 20:52:28 +0530

    Add files via upload

---
+++ inventory-monitoring/src/main/java/com/company/inventorymonitoring/InventoryMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventorymonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMonitoringApplication.class, args);
+	}
+
+}
